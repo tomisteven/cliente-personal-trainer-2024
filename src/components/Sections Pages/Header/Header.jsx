9e7911d5eebe68e93d3aa74d2b977593965fdceb
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-05-29 15:01:00 -0300

    instale el tailwind

--- src/components/Sections Pages/Header/Header.jsx
+++ src/components/Sections Pages/Header/Header.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
 
 export const Header = () => {
   return (
-    <header className='header-container'>
-        <div className='boton-home'>
-            <button>Home</button>
-        </div>
-        <div className='demas-botones'>
-            <button>Videos y Tips</button>
-            <button>Sobre mi</button>
-            <button>Planes</button>
-            <button>Blog</button>
-            <button>Carrito</button>
-
-        </div>
+    <header className="header-container">
+      <div className="boton-home">
+        <button>Home</button>
+      </div>
+      <div className="demas-botones">
+        <button>Videos y Tips</button>
+        <button>Sobre mi</button>
+        <button>Planes</button>
+        <button>Blog</button>
+        <button>Carrito</button>
+      </div>
     </header>
-   
-  )
-}
+  );
+};
